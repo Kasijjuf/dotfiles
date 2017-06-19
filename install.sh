91d80a279c397686532afa4ade2859d8a7e0bb3c
Kasijjuf <DavidPaul31@gmail.com>
2017-06-19 19:45:30 +0000

    Add gsmartcontrol config

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/
 wget -O ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 cat bashrc.append >> ~/.bashrc
 cp -T gitconfig ~/.gitconfig
+cp -T config/gsmartcontrol/gsmartcontrol.conf ~/.config/gsmartcontrol/gsmartcontrol.conf
