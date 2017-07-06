59e6707a11de69743bf94185d3e45089245afaee
Kasijjuf <DavidPaul31@gmail.com>
2017-07-06 16:22:48 +0000

    Reload .bashrc at end of install

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@ wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/
 wget -O ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 cat bashrc.append >> ~/.bashrc
 cp -T gitconfig ~/.gitconfig
-cp -T config/gsmartcontrol/gsmartcontrol.conf ~/.config/gsmartcontrol/gsmartcontrol.conf
+
+# TODO fix this line to create destination directory if necessary
+#cp -T config/gsmartcontrol/gsmartcontrol.conf ~/.config/gsmartcontrol/gsmartcontrol.conf
+
+. ~/.bashrc
