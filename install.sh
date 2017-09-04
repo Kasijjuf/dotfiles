b732b02b4c9547acd3d2bcf363662e8df8a4a7fd
Kasijjuf <DavidPaul31@gmail.com>
2017-09-04 17:03:36 -0500

    Add bash aliases

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 wget -O ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+cat bash_aliases.append >> ~/.bash_aliases
 cat bashrc.append >> ~/.bashrc
 cp -T gitconfig ~/.gitconfig
 
