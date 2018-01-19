503ebbbd407b62a955b5eb7794c59a325f95ae04
Kasijjuf <DavidPaul31@gmail.com>
2018-01-19 23:11:19 +0000

    Remove gsmartcontrol config
    It did not work reliably and was not applicable to CLI-only
    environments

--- install.sh
+++ install.sh
@@ -6,12 +6,4 @@ cat bash_aliases.append >> ~/.bash_aliases
 cat bashrc.append >> ~/.bashrc
 cp -T gitconfig ~/.gitconfig
 
-# gsmartcontrol settings
-# ======================
-#
-# Create gsmartcontrol config directory if necessary
-mkdir -p ~/.config/gsmartcontrol
-# TODO Rewrite to modify gsmartcontrol.conf instead of completely replacing it
-cp -t  ~/.config/gsmartcontrol config/gsmartcontrol/gsmartcontrol.conf
-
 . ~/.bashrc
