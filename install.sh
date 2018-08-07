314bc57fd4d51dd6fe1485ee1b7751a8c216dfa4
Kasijjuf <DavidPaul31@gmail.com>
2018-08-06 22:38:45 -0500

    Change how gitconfig is applied

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ ln -s -T /usr/share/bash-completion/completions/git ~/.git-completion.bash
 ln -s -T /usr/lib/git-core/git-sh-prompt ~/.git-prompt.sh
 cp -T --backup=numbered --update bash_aliases ~/.bash_aliases
 cat bashrc.append >> ~/.bashrc
-cp -T gitconfig ~/.gitconfig
+cp -T --backup=numbered --update gitconfig ~/.gitconfig
 
 . ~/.bashrc
