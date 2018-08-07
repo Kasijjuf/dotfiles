c0cbee04b68dbc40ae14b45f9e07f993cb9ab37f
Kasijjuf <DavidPaul31@gmail.com>
2018-08-07 11:44:58 -0500

    Add comments to install script

--- install.sh
+++ install.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
+# git-completion
 if [ ! -L ~/.git-completion.bash ]; then
     ln -s -T /usr/share/bash-completion/completions/git ~/.git-completion.bash
 fi
 
+# git-prompt
 if [ ! -L ~/.git-prompt.sh ]; then
     ln -s -T /usr/lib/git-core/git-sh-prompt ~/.git-prompt.sh
 fi
 
+# bash_aliases
 cp -T --backup=numbered --update bash_aliases ~/.bash_aliases
 
+# bashrc
 grep -q '\.\ \~/\.bashrc-append' ~/.bashrc || echo \
 "\nif [ -f ~/.bashrc-append ]; then\n    . ~/bashrc-append\nfi" >> ~/.bashrc
 cp -T --backup=numbered --update bashrc-append ~/.bashrc
 
+# gitconfig
 cp -T --backup=numbered --update gitconfig ~/.gitconfig
 
 . ~/.bashrc
