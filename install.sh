9d9950a3a4d42ddaa1a8014715da27cd531e9e95
Kasijjuf <DavidPaul31@gmail.com>
2018-08-07 11:28:48 -0500

    Change how bashrc additions are applied

--- install.sh
+++ install.sh
@@ -3,11 +3,17 @@
 if [ ! -L ~/.git-completion.bash ]; then
     ln -s -T /usr/share/bash-completion/completions/git ~/.git-completion.bash
 fi
+
 if [ ! -L ~/.git-prompt.sh ]; then
     ln -s -T /usr/lib/git-core/git-sh-prompt ~/.git-prompt.sh
 fi
+
 cp -T --backup=numbered --update bash_aliases ~/.bash_aliases
-cat bashrc.append >> ~/.bashrc
+
+grep -q '\.\ \~/\.bashrc-append' ~/.bashrc || echo \
+"\nif [ -f ~/.bashrc-append ]; then\n    . ~/bashrc-append\nfi" >> ~/.bashrc
+cp -T --backup=numbered --update bashrc-append ~/.bashrc
+
 cp -T --backup=numbered --update gitconfig ~/.gitconfig
 
 . ~/.bashrc
